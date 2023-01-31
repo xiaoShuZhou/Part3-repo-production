d802f0d59fd9de47336f470067430a39f3db4772
Sean Zhou <51042748+xiaoShuZhou@users.noreply.github.com>
2023-01-31 10:31:29 +0200

    configure prot in production env

--- phoneapi/index.js
+++ phoneapi/index.js
@@ -108,8 +108,13 @@ app.put('/api/persons/:id', (request, response) => {
 
 
 
-const PORT = 3001;
+// const PORT = 3001;
 
+// app.listen(PORT, () => {
+//    console.log(`Server is running on PORT: ${PORT}`);
+// });
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-   console.log(`Server is running on PORT: ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})

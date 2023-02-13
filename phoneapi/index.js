7100913fc033b43cc345fe5f71bfc77716f710ae
Sean Zhou <51042748+xiaoShuZhou@users.noreply.github.com>
2023-02-13 12:45:05 +0200

    use static file

--- phoneapi/index.js
+++ phoneapi/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-
+app.use(express.static('build'))
 require('dotenv').config()
 const Person= require('./models/person')
 
